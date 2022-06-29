45765780b4ea2f97ce9d6653cdd06d5490b0cfa8
faizaan1904 <68111240+faizaan1904@users.noreply.github.com>
2022-06-29 20:16:43 +0530

    Create FindDuplicate.cpp
    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
    
    There is only one repeated number in nums, return this repeated number.

---
+++ FindDuplicate.cpp
@@ -0,0 +1,11 @@
+int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        for(;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1])
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
